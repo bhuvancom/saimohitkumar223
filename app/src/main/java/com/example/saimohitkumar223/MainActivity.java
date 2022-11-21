2f8a32cc9b6654f22e2f78e193f4c1d445e6e551
Bhuvaneshvar <bhuvaneshvar8@gmail.com>
2022-11-21 15:19:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saimohitkumar223/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.saimohitkumar223;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submit_223();
+    }
+
+    private void submit_223(){
+
+    }
+}
